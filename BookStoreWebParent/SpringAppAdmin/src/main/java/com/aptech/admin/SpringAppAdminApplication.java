662df45a7c46020a5069bd5d7723134f1d608e14
qnguyen95 <quangtri.nguyen@dxc.com>
2022-08-03 11:19:21 +0700

    #Add Entity class User && Paging Page

--- BookStoreWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/SpringAppAdminApplication.java
+++ BookStoreWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/SpringAppAdminApplication.java
@@ -1,9 +1,11 @@
-package com.aptech;
+package com.aptech.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.aptech.common.entity"})
 public class SpringAppAdminApplication {
 
 	public static void main(String[] args) {
