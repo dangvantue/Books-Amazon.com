619c8a867d558c351afa3758334d4bf4fe31f364
qnguyen95 <quangtri.nguyen@dxc.com>
2022-09-07 12:05:27 +0700

    #Code Internationalization(i18n)

--- BookStoreWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/MainController.java
+++ BookStoreWebParent/SpringAppAdmin/src/main/java/com/aptech/admin/MainController.java
@@ -2,6 +2,11 @@ package com.aptech.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,7 +14,25 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @Controller
 public class MainController {
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage(HttpServletRequest request ) {
+		Locale currentLocate = request.getLocale();
+		String countryCode = currentLocate.getCountry();
+		String countryName = currentLocate.getDisplayCountry();
+		
+		String langCode = currentLocate.getLanguage();
+		String langName = currentLocate.getDisplayLanguage();
+		
+		System.out.println(countryCode + ": " + countryName);
+		System.out.println(langCode + ": " + langName);
+		
+		System.out.println("==========================");
+		String[] languages = Locale.getISOLanguages();
+		
+		for (String language : languages) {
+			Locale locate = new Locale(language);
+			System.out.println(language + ": " + locate.getDisplayLanguage());
+		}
+		
 		return "index";
 	}
 
